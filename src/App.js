0a8600b2be0990a34748dd93badb5ad7267fc0e8
Aishwarya Venugopalan <aishwaryavenugopalan@Aishwaryas-MacBook-Pro.local>
2021-07-11 18:08:13 -0700

    initial  commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Form from './components/Form'
+import React, {useState} from 'react';
+import CardList from './components/CardList';
+
+function App() {
+
+  const addNewCard = (card) => {
+    setCards([...cards, card])
+  }
+  
+  const [cards,setCards] = useState([])
+  return (
+    <div>
+      <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">Search a GitHub User</h1>
+      <Form onSubmit={addNewCard}/>
+      <CardList cards={cards} />
+    </div>
+  );
+}
+
+export default App;
